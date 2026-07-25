cd23a330a64993f6443b30fe97c7ec88f26bd88a
GeorgScherzer <g.scherzer@medeqon.com>
2026-07-25 18:42:24 +0200

    Wasserzeichen

--- build.py
+++ build.py
@@ -436,7 +436,7 @@ BODY_PRODUKTE = '''<section class="m-page-hero">
 
     <div class="m-acc">
       <details class="m-ac" id="persoenlicher-strahlenschutz">
-        <summary><span class="m-ac-num">01</span><span class="m-ac-title">Persönlicher Strahlenschutz</span>''' + CHEV + '''</summary>
+        <summary><span class="m-ac-num">01</span><span class="m-ac-title">Persönlicher Strahlenschutz</span><img class="m-ac-mfr m-ac-mfr--rothband" src="assets/brands/rothband.png" alt="ROTHBAND" loading="lazy">''' + CHEV + '''</summary>
         <div class="m-ac-body">
           <p class="m-ac-lead">Strahlenschutzbekleidung für den direkten Personenschutz – Schürzen, Zweiteiler, Schilddrüsenschutz und ergänzendes Zubehör. Fokus auf Ergonomie, Gewichtsverteilung, Schutzleistung und Tragekomfort.</p>
           <div class="m-acc m-acc-nested">
@@ -504,7 +504,7 @@ BODY_PRODUKTE = '''<section class="m-page-hero">
       </details>
 
       <details class="m-ac" id="ss-aufbewahrung">
-        <summary><span class="m-ac-num">02</span><span class="m-ac-title">Aufbewahrung</span>''' + CHEV + '''</summary>
+        <summary><span class="m-ac-num">02</span><span class="m-ac-title">Aufbewahrung</span><img class="m-ac-mfr m-ac-mfr--rothband" src="assets/brands/rothband.png" alt="ROTHBAND" loading="lazy">''' + CHEV + '''</summary>
         <div class="m-ac-body">
           <p class="m-ac-lead">Ständer, Schwenkarme und Bügel zur sicheren, platzsparenden Aufbewahrung von Strahlenschutzschürzen.</p>
           <p class="m-pl-count">''' + str(_ss_count("aufbewahrung")) + ''' Produkte verfügbar</p>
@@ -515,7 +515,7 @@ BODY_PRODUKTE = '''<section class="m-page-hero">
       </details>
 
       <details class="m-ac" id="mobiler-strahlenschutz">
-        <summary><span class="m-ac-num">03</span><span class="m-ac-title">Mobiler Strahlenschutz</span>''' + CHEV + '''</summary>
+        <summary><span class="m-ac-num">03</span><span class="m-ac-title">Mobiler Strahlenschutz</span><img class="m-ac-mfr m-ac-mfr--kenex" src="assets/brands/kenex.png" alt="KENEX" loading="lazy">''' + CHEV + '''</summary>
         <div class="m-ac-body">
           <p class="m-ac-lead">Fahrbare, frei positionierbare Röntgenschutzsysteme für flexible Einsätze im OP und in der interventionellen Radiologie – vom Hersteller KENEX.</p>
           <p class="m-pl-count">''' + str(_kenex_count("mobil")) + ''' Modelle verfügbar</p>
@@ -526,7 +526,7 @@ BODY_PRODUKTE = '''<section class="m-page-hero">
       </details>
 
       <details class="m-ac" id="deckenmontierter-strahlenschutz">
-        <summary><span class="m-ac-num">04</span><span class="m-ac-title">Deckenmontierter Strahlenschutz</span>''' + CHEV + '''</summary>
+        <summary><span class="m-ac-num">04</span><span class="m-ac-title">Deckenmontierter Strahlenschutz</span><img class="m-ac-mfr m-ac-mfr--kenex" src="assets/brands/kenex.png" alt="KENEX" loading="lazy">''' + CHEV + '''</summary>
         <div class="m-ac-body">
           <p class="m-ac-lead">Aufgehängte Überkopf-Schutzschilde zur deutlichen Reduktion der Streustrahlung im Arbeitsbereich.</p>
           <span class="m-ac-src">Produkte folgen in Kürze.</span>
@@ -534,7 +534,7 @@ BODY_PRODUKTE = '''<section class="m-page-hero">
       </details>
 
       <details class="m-ac" id="tischmontierter-strahlenschutz">
-        <summary><span class="m-ac-num">05</span><span class="m-ac-title">Tischmontierter Strahlenschutz</span>''' + CHEV + '''</summary>
+        <summary><span class="m-ac-num">05</span><span class="m-ac-title">Tischmontierter Strahlenschutz</span><img class="m-ac-mfr m-ac-mfr--kenex" src="assets/brands/kenex.png" alt="KENEX" loading="lazy">''' + CHEV + '''</summary>
         <div class="m-ac-body">
           <p class="m-ac-lead">Am Untersuchungstisch montierte Schutzschilde für die interventionelle Radiologie.</p>
           <span class="m-ac-src">Produkte folgen in Kürze.</span>
